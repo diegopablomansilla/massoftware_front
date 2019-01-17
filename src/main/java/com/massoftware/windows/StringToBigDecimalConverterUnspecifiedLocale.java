0b22d4f795ee0d0db21848cbe317cfe5fe722cb2
Diego Pablo Mansilla <diegopablomansilla@gmail.com>
2019-01-17 11:52:36 -0300

    Zonas - falta terminarlo un poquito, columnas bonificacion y recargo no estan en la grilla

--- src/main/java/com/massoftware/windows/StringToBigDecimalConverterUnspecifiedLocale.java
+++ src/main/java/com/massoftware/windows/StringToBigDecimalConverterUnspecifiedLocale.java
@@ -1,6 +1,8 @@
 package com.massoftware.windows;
 
 import java.math.BigDecimal;
+import java.text.DecimalFormat;
+import java.text.NumberFormat;
 import java.util.Locale;
 
 import com.vaadin.data.util.converter.StringToBigDecimalConverter;
@@ -12,10 +14,21 @@ public class StringToBigDecimalConverterUnspecifiedLocale extends
 	 * 
 	 */
 	private static final long serialVersionUID = 5743612309620235994L;
+	
+	 @Override
+	    protected NumberFormat getFormat(Locale locale) {
+	        NumberFormat numberFormat = super.getFormat(locale.US);
+	        if (numberFormat instanceof DecimalFormat) {
+	            ((DecimalFormat) numberFormat).setParseBigDecimal(true);
+	        }
+
+	        return numberFormat;
+	    }
 
 	public String convertToPresentation(BigDecimal value,
 			Class<? extends String> targetType, Locale locale)
 			throws ConversionException {
+		
 		if (value == null) {
 			return null;
 		}
